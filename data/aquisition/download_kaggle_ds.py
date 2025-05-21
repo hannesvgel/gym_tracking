eb09cd9737a7aa507773500aee712761ea40a2e9
edoardo coluccino <edoardo.coluccino@mail.polimi.it>
2025-05-21 12:36:32 +0200

    fix

--- data/aquisition/download_kaggle_ds.py
+++ data/aquisition/download_kaggle_ds.py
@@ -1,8 +1,8 @@
 import kagglehub
 
 # run this file to downloade the below datasets from kaggle
-path_workout_videos = kagglehub.dataset_download("hasyimabdillah/workoutfitness-video")
-print("Path to dataset files:", path_workout_videos)
+'''path_workout_videos = kagglehub.dataset_download("hasyimabdillah/workoutfitness-video")
+print("Path to dataset files:", path_workout_videos)'''
 
-'''path_workout_photos = kagglehub.dataset_download("hasyimabdillah/workoutexercises-images")
-print("Path to dataset files:", path_workout_photos)'''
+path_workout_photos = kagglehub.dataset_download("hasyimabdillah/workoutexercises-images")
+print("Path to dataset files:", path_workout_photos)
