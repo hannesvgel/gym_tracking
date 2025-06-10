c4c1baf82b4236908165d4fc7ef0da26979dc23e
Hannes Vogel <hannes.vogel@tum.de>
2025-06-10 08:09:54 +0200

    fix: updated classes

--- src/realtime_inference/thirty_frames/single_exercise_classification/lstm_multiclass3.py
+++ src/realtime_inference/thirty_frames/single_exercise_classification/lstm_multiclass3.py
@@ -1,6 +1,6 @@
 from fct_exercise_classification_per_30_frames import classify_per_30_frames
 
 MODEL_PATH = "skeleton_lstm_multiclass3.h5"
-CLASSES = ["push_up","split_squat","pull_up"]
+CLASSES      = ["pull_up", "push_up", "split_squat"]  # pull_up: 0, push_up: 1, split_squat: 2
 
 classify_per_30_frames(MODEL_PATH, CLASSES)
