a7816e99d5862b8ba15baf3bddb44345f01742be
Hannes Vogel <hannes.vogel@tum.de>
2025-06-17 10:59:19 +0200

    updated kaggle ds download

--- data/aquisition/download_kaggle_ds.py
+++ data/aquisition/download_kaggle_ds.py
@@ -4,5 +4,8 @@ import kagglehub
 '''path_workout_videos = kagglehub.dataset_download("hasyimabdillah/workoutfitness-video")
 print("Path to dataset files:", path_workout_videos)'''
 
-path_workout_photos = kagglehub.dataset_download("hasyimabdillah/workoutexercises-images")
-print("Path to dataset files:", path_workout_photos)
+'''path_workout_photos = kagglehub.dataset_download("hasyimabdillah/workoutexercises-images")
+print("Path to dataset files:", path_workout_photos)'''
+
+path_workout_videos_2 = kagglehub.dataset_download("philosopher0808/gym-workoutexercises-video")
+print("Path to dataset files:", path_workout_videos_2)
